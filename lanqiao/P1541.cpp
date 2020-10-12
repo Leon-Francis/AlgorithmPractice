eb148198ef813beb11f9af131f7b0a4e3a5d56f3
Leon-Francis <15290552788@163.com>
2020-10-13 00:04:44 +0800

    2020/10/12 check

--- lanqiao/P1541.cpp
+++ lanqiao/P1541.cpp
@@ -56,24 +56,24 @@ using namespace std;
 //     return 0;
 // }
 // #endif
-int dp[40][40][40][40];
+int dp[41][41][41][41];
 int n, m;
 int score[351];
-int card[4] = {0, 0, 0, 0};
+int card[5] = {0, 0, 0, 0, 0};
 int main(int argc, char const *argv[])
 {
     cin >> n >> m;
-    for (int i = 0; i < n; i++)
+    for (int i = 1; i <= n; i++)
     {
         cin >> score[i];
     }
-    for (int i = 0; i < m; i++)
+    for (int i = 1; i <= m; i++)
     {
         int a;
         cin >> a;
         card[a]++;
     }
-    dp[0][0][0][0] = score[0];
+    dp[0][0][0][0] = score[1];
     for (int a = 0; a <= card[1]; a++)
         for (int b = 0; b <= card[2]; b++)
             for (int c = 0; c <= card[3]; c++)
