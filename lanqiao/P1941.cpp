f34e3b783aa8a988882c0794eba765e4f0cabf7b
Leon-Francis <15290552788@163.com>
2020-10-15 22:42:53 +0800

    2020/10/15 check

---
+++ lanqiao/P1941.cpp
@@ -0,0 +1,12 @@
+/*
+ * @Author: Leon-Francis
+ * @Contact: 15290552788@163.com
+ * @Date: 2020-10-15 22:07:34
+ * @LastEditTime: 2020-10-15 22:07:35
+ * @LastEditors: Leon-Francis
+ * @Description: P1941 飞扬的小鸟
+ * @FilePath: /AlgorithmPractice/lanqiao/P1941.cpp
+ * @(C)Copyright 2019-2020, Leon-Francis
+ */
+//状态是高度
+//题目对年幼的我来说过于复杂，跳过
--- suanfake/01beibao.cpp
+++ suanfake/01beibao.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 using namespace std;
-
+#if 0
 void PrintResult(int form[7][13], int W[7])
 {
     int j = 12;
@@ -55,5 +55,34 @@ int main(int argc, char const *argv[])
         cout << endl;
     }
     PrintResult(form, W);
+    return 0;
+}
+#endif
+//使用一维数组来进行尝试
+#include <iostream>
+using namespace std;
+int main(int argc, char const *argv[])
+{
+    int W[7] = {0, 3, 5, 2, 6, 4, 7};
+    int P[7] = {0, 4, 7, 5, 11, 3, 8};
+    int dp[13] = {0};
+    for (int i = 0; i <= 12; i++)
+    {
+        if (i >= W[1])
+        {
+            dp[i] = P[1];
+        }
+    }
+    for (int i = 2; i <= 6; i++)
+    {
+        for (int j = 12; j >= 0; j--)
+        {
+            if (j >= W[i])
+            {
+                dp[j] = max(dp[j], dp[j - W[i]] + P[i]);
+            }
+        }
+    }
+    cout << dp[12] << endl;
     return 0;
 }
